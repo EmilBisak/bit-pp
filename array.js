9b785524a140d6481aaf0041e16cd7e38bb01003
BIT Student <classroom@bgit.rs>
2018-04-23 12:03:15 +0200

    Arrays and loops

---
+++ array.js
@@ -0,0 +1,55 @@
+//Arrays
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+console.log(months[5],months[9],months[0]);
+
+
+
+var days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+console.log(days[6]);
+
+
+
+var nums = [2,-4,5,-2,-11];
+console.log(nums[1],nums[3],nums[4]);
+
+
+
+var nums2 = [5,15,-5,20,12,18,72,14,9]
+// console.log(nums2[1],nums2[4],nums2[5],nums2[6],nums2[8]);
+if (nums2[0]%3===0) {
+    console.log(nums2[0]);
+}
+ if(nums2[1]%3===0) {
+    console.log(nums2[1]);
+}
+ if(nums2[2]%3===0) {
+    console.log(nums2[2]);
+}
+ if(nums2[3]%3===0) {
+    console.log(nums2[3]);
+}
+ if(nums2[4]%3===0) {
+    console.log(nums2[4]);
+}
+ if(nums2[5]%3===0) {
+    console.log(nums2[5]);
+}
+ if(nums2[6]%3===0) {
+    console.log(nums2[6]);
+}
+ if(nums2[7]%3===0) {
+    console.log(nums2[7]);
+}
+if(nums2[8]%3===0) {
+    console.log(nums2[8]);
+}
+
+
+
+var nums3 = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+console.log(nums3[0][3]);
+console.log(nums3[2]);
+console.log(nums3[2][1]);
+
+
+
---
+++ loops.js
@@ -0,0 +1,119 @@
+//Loops
+
+// While
+
+// var i = 0;
+
+// while (i < 10) {
+//     i++;
+
+// }
+
+// console.log(i);
+
+
+// var numbers = [3, 4, -1, 5, -2, 0, -6];
+// var i = 0;
+// while (i < numbers.length) {
+
+//     if (numbers[i] < 0) {
+//         console.log(numbers[i]);
+//     }
+//     i++;
+// }
+
+
+// var y = numbers.length - 1;
+// while (y >= 0) {
+//     console.log(numbers[y]);
+//     y--;
+// }
+
+
+// zadatak 1 
+
+// var i = 0;
+// var nums = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// var odd = [];
+// var even= [];
+// var oddIndex = 0;
+// var evenIndex = 0;
+
+// while (i < nums.length) {
+//     if (nums[i] % 2 === 0) {
+//         even[evenIndex++] = nums[i];
+//     } else {
+//         odd[oddIndex++] = nums[i];   
+//     }
+//     i++;
+// }
+// console.log("odd:", odd);
+// console.log("even:", even);
+
+
+
+//zadatak2
+
+// var i = 0;
+// var sum = 0;
+// while (i < 1000) {
+
+//     if (i % 3 === 0 && i % 5 === 0) {
+//        sum = sum + i; 
+//     }
+//     i++;
+// }
+
+// console.log(sum);
+
+
+//zadatak3
+
+// var i = 0;
+// var a = [1,3,4,6,8];
+// var sum2 = 0;
+// var pro = 1;
+
+// while (i < a.length) {
+//     sum2 += a[i];
+//     pro *= a[i];
+//     i++;
+
+// }
+
+// console.log(sum2);
+// console.log(pro);
+
+
+
+//zadatak 4
+
+// var i = 0;
+// var x = ['1', 'A', 'B', "c", "r", true, NaN, undefined];
+// var singleString = "";
+
+// while (i < x.length) {
+//     singleString = 
+// }
+
+
+
+// Do-while
+// var i = 10;
+// do {
+//     i++;
+// } while (i<10);
+
+// console.log(i);
+
+
+
+//For 
+
+var nums = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+var sum = 0;
+for (let i = 0; i < nums.length; i++) {
+    sum += nums[i]
+
+}
+console.log(sum);
