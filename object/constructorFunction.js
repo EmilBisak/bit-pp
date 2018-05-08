6eac617fdc5d59852205b33b45f7eb67de36a3d0
BIT Student <clasroom@bgit.rs>
2018-05-08 18:00:51 +0200

    object

---
+++ object/constructorFunction.js
@@ -0,0 +1,23 @@
+// function Person(name, surname, balance) {
+//     this.name = name;
+//     this.surname = surname;
+//     this.sayName = function () {
+//         console.log("Hi, my name is " + this.name + " " + this.surname);
+//     };
+//     this.balance = balance;
+
+// }
+
+// var natalija = new Person("natalija", "mirkovic", 0);
+// console.log(natalija);
+// natalija.sayName();
+
+// var emil = new Person("Emil", "Bisak", 12000);
+// console.log(emil);
+// emil.sayName();
+
+var date = new Date();
+
+console.log(date.getFullYear());
+
+
---
+++ object/practical_built_in_objects.js
@@ -0,0 +1,69 @@
+/* Task 1
+    Write a function to convert a number from one base (radix) to another. 
+    Hint: Use one of the built-in functions and toString method of one of the built-in Object Wrappers;
+
+    'ff', 16, 8 -> 377 */
+
+// function convertBase(string, baseInput, baseOutput) {
+//     var number = parseInt(string, baseInput);
+//     var convertedBase = number.toString(baseOutput);
+//     return convertedBase;
+// }
+
+// console.log(convertBase("ff", 16, 8));
+
+
+
+
+// task 2 
+// Write a JavaScript function that reverses a number. typeof result of the function should be “number”.
+
+// 	12345 -> 54321
+
+
+
+// function reverseNumbers(string) {
+//     return parseInt(string.split("").reverse().join(""))
+// }
+
+// console.log(reverseNumbers("12345"));
+
+
+
+// task 3
+// Write a JavaScript function that returns a passed string with letters in alphabetical order. 
+// Note: Assume punctuation, numbers and symbols are not included in the passed string.
+// “Webmaster” -> “abeemrstw”
+
+// function sortLetters(string) {
+//     return string.toLowerCase().split("").sort().join("");
+// }
+
+// console.log(sortLetters("Webmaster"));
+
+
+// task 4
+// Write a function to alphabetize words of a given string. Alphabetizing a string means rearranging the letters so they are sorted from A to Z.
+
+// 	"Republic Of Serbia" -> "Rbceilpu Of Sabeir"
+
+
+function sortAlphabeticly(string) {
+
+    var newArray = string.split(" ");
+    var newString = "";
+    var newArray2 = [];
+
+    for (var i = 0; i < newArray.length; i++) {
+        newString = newArray[i].split("").sort().join("");
+        newArray2 += newString + " ";
+    }
+
+    return newArray2;
+}
+
+console.log(sortAlphabeticly("Republic Of Serbia"));
+ 
+
+
+
